51435ce4e932035a373f35aa72011474db5ae618
60071jimmy <60071jimmy@gmail.com>
2017-08-01 11:24:34 +0800

    新增註解
    新增註解

--- src/api/Detect.cpp
+++ src/api/Detect.cpp
@@ -1,4 +1,5 @@
-#include <api/Detect.hpp>
+//-----include header files, 引入標頭檔-----
+#include <api/Detect.hpp>														//	include <api/Detect.hpp> header file, 引入<api/Detect.hpp>標頭檔
 #include "opencv2/imgproc/imgproc.hpp"
 
     void MyGammaCorrection(cv::Mat& src, cv::Mat& dst, float fGamma)  
@@ -52,131 +53,142 @@
 
 namespace api {
     
-Detect::Detect()
-    : opened(false)
-{
-}
+	Detect::Detect()
+		: opened(false)
+	{
+	}
 
-void Detect::open() {
-    using namespace std;
-    using namespace dlib;
-    if (opened)
-        return;
-    try
-    {
-        detector = get_frontal_face_detector();
-        deserialize("shape_predictor_68_face_landmarks.dat") >> sp;
-        deserialize("Training_official.dat") >> net;
+	void Detect::open() {
+		using namespace std;
+		using namespace dlib;
+		if (opened)
+			return;
+		try
+		{
+			detector = get_frontal_face_detector();
+			deserialize("shape_predictor_68_face_landmarks.dat") >> sp;
+			deserialize("Training_official.dat") >> net;
 
-        deserialize("database_name.dat") >> name;
-        deserialize("database_decr.dat") >> database_decr;
-        opened = true;
-    }
-    catch(std::exception& e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
-}
+			deserialize("database_name.dat") >> name;
+			deserialize("database_decr.dat") >> database_decr;
+			opened = true;
+		}
+		catch(std::exception& e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+	}
 
-bool Detect::is_opened() {
-    return opened;
-}
+	bool Detect::is_opened() {
+		return opened;
+	}
 
-std::vector<std::string> Detect::detect_user(cv::Mat& image)
-{
-    using namespace std;
-    using namespace dlib;
-    std::vector<cv::Mat> tmp;
-    cv::Mat image_temp;
-    try
-    {
+	std::vector<std::string> Detect::detect_user(cv::Mat& image)
+	{
+		using namespace std;
+		using namespace dlib;
+		std::vector<cv::Mat> tmp;
+		cv::Mat image_temp;
+		try
+		{
 
-        cv::cvtColor(image, image_temp, CV_BGR2HSV);
-	cv::split(image_temp,tmp);
-	cv::medianBlur(tmp[2], tmp[2], 3);
-        cv::equalizeHist(tmp[2], tmp[2]);
-        cv::merge(tmp,image_temp);
-        cv::cvtColor(image_temp, image, CV_HSV2BGR);
+			cv::cvtColor(image, image_temp, CV_BGR2HSV);
+			cv::split(image_temp,tmp);
+			cv::medianBlur(tmp[2], tmp[2], 3);
+			cv::equalizeHist(tmp[2], tmp[2]);
+			cv::merge(tmp,image_temp);
+			cv::cvtColor(image_temp, image, CV_HSV2BGR);
 
-	dlib::matrix<rgb_pixel> img;
-        std::vector<string> person;
+			dlib::matrix<rgb_pixel> img;
+			std::vector<string> person;
 
-        cv_image<bgr_pixel> cimg(image);
-        assign_image(img,cimg);
-        std::vector<matrix<rgb_pixel>> faces;
+			cv_image<bgr_pixel> cimg(image);
+			assign_image(img,cimg);
+			std::vector<matrix<rgb_pixel>> faces;
+			
+			//bool flag = true;
+			//std::thread mThread(getKey, cvWaitKey(20), &count, &flag, img);
+			//mThread.join();
 
-        for(auto face_rect : detector(img))
-        {
-            auto shape = sp(img, face_rect);
-            matrix<rgb_pixel> face_chip;
-            extract_image_chip(img, get_face_chip_details(shape,150,0.25), face_chip);
-            faces.push_back(move(face_chip));
-	    //draw_rectangle(img, face, rgb_pixel(255,0,0), 1);         
-            cv::rectangle(
-                image,
-                cv::Point(face_rect.left(),face_rect.top()),
-                cv::Point(face_rect.right(),face_rect.bottom()),
-                cv::Scalar(255,0,0));
-        }
+			for(auto face_rect : detector(img))
+			{
+				auto shape = sp(img, face_rect);
+				matrix<rgb_pixel> face_chip;
+				extract_image_chip(img, get_face_chip_details(shape,150,0.25), face_chip);
+				faces.push_back(move(face_chip));
+				//draw_rectangle(img, face, rgb_pixel(255,0,0), 1);         
+				cv::rectangle(
+					image,
+					cv::Point(face_rect.left(),face_rect.top()),
+					cv::Point(face_rect.right(),face_rect.bottom()),
+					cv::Scalar(255,0,0));
+					
+					
+			}
 
-        std::vector<matrix<float,0,1>> face_descriptors = net(faces);
-        std::vector<int> match_count(faces.size());
-        std::vector<int> candidate(database_decr.size());
-        std::vector<float> distance_list;
+			std::vector<matrix<float,0,1>> face_descriptors = net(faces);
+			std::vector<int> match_count(faces.size());
+			std::vector<int> candidate(database_decr.size());
+			std::vector<float> distance_list;
 
-        for(size_t a = 0; a < faces.size(); ++a)
-        {
-            int preserve_index = 0;
-            for(size_t b = 0; b < database_decr.size(); ++b)
-            {
-                float distant = length(face_descriptors[a]-database_decr[b]);
-                distance_list.push_back(distant);
-                if(distant < 0.33)
-                {
-                    preserve_index = b;
-                    match_count[a]++;
-                    candidate[b]++;
-                }
-            }
-            if(match_count[a] == 0)
-            {
-                // std::cerr << "Not recognized!" << std::endl;
-		person.push_back("NOT_RECOGNIZED");
-            }
-            else if(match_count[a] != 1)
-            {
-                int index = 0;
-                float distance_temp = distance_list[0];
-                for(size_t c = 0; c < candidate.size(); c++)
-                {
-                    if(candidate[c] != 0)
-                    {
-                        if(distance_temp > distance_list[c])
-                        {
-                            index = c;
-                            distance_temp = distance_list[c];
-                        }
-                    }
-                }
-                // std::cerr << "\"" << name[index] << "\" is here!" << "   with distance : " << distance_list[index] << std::endl;
-                person.push_back(name[index]);
-            }
-            else if(match_count[a] == 1)
-            {
-                // std::cerr << "\"" << name[preserve_index] << "\" is here!" << "   with distance : " << distance_list[preserve_index] << std::endl;
-                person.push_back(name[preserve_index]);
-            }
-        }
-        // if(faces.size() != 0)
-        //           std::cerr << "----------------------------------------" << std::endl;
-	person.push_back("\n");
-        return person;
-    }
-    catch (std::exception& e)
-    {
-        cerr << e.what() << endl;
-    }
-    return {};
-}
+			for(size_t a = 0; a < faces.size(); ++a)
+			{
+				cv::Mat save = toMat(faces[a]);
+				cv::imwrite(FILE_NAME + std::to_string(a) + ".jpg", save);
+				
+				int preserve_index = 0;
+				for(size_t b = 0; b < database_decr.size(); ++b)
+				{
+					float distant = length(face_descriptors[a]-database_decr[b]);
+					distance_list.push_back(distant);
+					if(distant < 0.33)
+					{
+						preserve_index = b;
+						match_count[a]++;
+						candidate[b]++;
+					}
+				}
+				if(match_count[a] == 0)
+				{
+					// std::cerr << "Not recognized!" << std::endl;
+					person.push_back("NOT_RECOGNIZED");
+				}
+				else if(match_count[a] != 1)
+				{
+					int index = 0;
+					float distance_temp = distance_list[0];
+					for(size_t c = 0; c < candidate.size(); c++)
+					{
+						if(candidate[c] != 0)
+						{
+							if(distance_temp > distance_list[c])
+							{
+								index = c;
+								distance_temp = distance_list[c];
+							}
+						}
+					}
+					// std::cerr << "\"" << name[index] << "\" is here!" << "   with distance : " << distance_list[index] << std::endl;
+					person.push_back(name[index]);
+				}
+				else if(match_count[a] == 1)
+				{
+					// std::cerr << "\"" << name[preserve_index] << "\" is here!" << "   with distance : " << distance_list[preserve_index] << std::endl;
+					person.push_back(name[preserve_index]);
+				}
+			}
+			// if(faces.size() != 0)
+			//           std::cerr << "----------------------------------------" << std::endl;
+		person.push_back("\n");
+			return person;
+		}
+		catch (std::exception& e)
+		{
+			cerr << e.what() << endl;
+		}
+		return {};
+	}
+	
+	
 
 } // namespace api
