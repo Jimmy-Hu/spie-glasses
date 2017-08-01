176ffcd559a95ae807dbc3edb14ffcc675c1ef3d
60071jimmy <60071jimmy@gmail.com>
2017-08-01 22:08:17 +0800

    調整排版
    調整排版

--- src/api/Detect.cpp
+++ src/api/Detect.cpp
@@ -32,25 +32,23 @@ void MyGammaCorrection(cv::Mat& src, cv::Mat& dst, float fGamma)
 			}
             break;
         }
-        case 3:   
-                {  
+        case 3:
+        {  
       
-                    MatIterator_<Vec3b> it, end;  
-                    for( it = dst.begin<Vec3b>(), end = dst.end<Vec3b>(); it != end; it++ )  
-                    {  
-                        //(*it)[0] = pow((float)(((*it)[0])/255.0), fGamma) * 255.0;  
-                        //(*it)[1] = pow((float)(((*it)[1])/255.0), fGamma) * 255.0;  
-                        //(*it)[2] = pow((float)(((*it)[2])/255.0), fGamma) * 255.0;  
-                        (*it)[0] = lut[((*it)[0])];  
-                        (*it)[1] = lut[((*it)[1])];  
-                        (*it)[2] = lut[((*it)[2])];  
-                    }  
-      
-                    break;  
-      
-                }  
+            MatIterator_<Vec3b> it, end;  
+            for( it = dst.begin<Vec3b>(), end = dst.end<Vec3b>(); it != end; it++ )  
+            {  
+                //(*it)[0] = pow((float)(((*it)[0])/255.0), fGamma) * 255.0;  
+                //(*it)[1] = pow((float)(((*it)[1])/255.0), fGamma) * 255.0;  
+                //(*it)[2] = pow((float)(((*it)[2])/255.0), fGamma) * 255.0;  
+                (*it)[0] = lut[((*it)[0])];  
+                (*it)[1] = lut[((*it)[1])];  
+                (*it)[2] = lut[((*it)[2])];  
+            }  
+            break;  
         }  
     }  
+}  
 
 namespace api {
     
