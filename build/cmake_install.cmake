aa55ae8be84818758956751eea57f0f4cdf4b32f
60071jimmy <60071jimmy@gmail.com>
2017-08-01 11:44:42 +0800

    重新編譯
    重新編譯

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: E:/msys2/home/jimmy/proj/spie-glasses
+# Install script for directory: D:/GitHub/spie-glasses
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "E:/msys2/home/jimmy/proj/spie-glasses/build/install")
+  set(CMAKE_INSTALL_PREFIX "D:/GitHub/spie-glasses/build/install")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -35,5 +35,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "E:/msys2/home/jimmy/proj/spie-glasses/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/GitHub/spie-glasses/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
