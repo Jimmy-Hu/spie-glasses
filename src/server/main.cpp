5e1eba6fedf2f6e0748ef0d60bd9f90b0c5252e3
60071jimmy <60071jimmy@gmail.com>
2017-08-02 11:17:13 +0800

    新增註解
    新增註解

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -4,23 +4,23 @@
 #include "session.hpp"															//	include "session.hpp" header file, 引入"session.hpp"標頭檔
 #include "session_manager.hpp"													//	include "session_manager.hpp" header file, 引入"session_manager.hpp"標頭檔
 // internal api
-#include <api/Detect.hpp>
+#include <api/Detect.hpp>														//	include <api/Detect.hpp> header file, 引入<api/Detect.hpp>標頭檔
 // external api
-#include <boost/asio.hpp>
-#include <boost/system/error_code.hpp>
-#include <msgpack.hpp>
-#include <json.hpp>
+#include <boost/asio.hpp>														//	引入<boost/asio.hpp>標頭檔
+#include <boost/system/error_code.hpp>											//	引入<boost/system/error_code.hpp>標頭檔
+#include <msgpack.hpp>															//	引入<msgpack.hpp>標頭檔
+#include <json.hpp>																//	引入<json.hpp>標頭檔
 // standard api
-#include <cstdlib>
-#include <deque>
-#include <iostream>
-#include <list>
-#include <memory>
-#include <set>
-#include <map>
-#include <utility>
-#include <thread>
-#include <algorithm>
+#include <cstdlib>																//	引入<cstdlib>標頭檔
+#include <deque>																//	引入<deque>標頭檔
+#include <iostream>																//	引入<iostream>標頭檔
+#include <list>																	//	引入<list>標頭檔
+#include <memory>																//	引入<memory>標頭檔
+#include <set>																	//	引入<set>標頭檔
+#include <map>																	//	引入<map>標頭檔
+#include <utility>																//	引入<utility>標頭檔
+#include <thread>																//	引入<thread>標頭檔
+#include <algorithm>															//	引入<algorithm>標頭檔
 
 int main(int argc, char* argv[])
 {
