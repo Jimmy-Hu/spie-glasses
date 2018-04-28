c5da6b036fa177d7df804a31381734758e04e1a3
ChienYu Lin <cy20lin@gmail.com>
2018-04-28 21:54:01 +0800

    Update .archer.d layer /ubuntu/lib/boost

--- .archer.d/layer/ubuntu/lib/boost/layer.bash
+++ .archer.d/layer/ubuntu/lib/boost/layer.bash
@@ -18,14 +18,14 @@ layer_is_installed() {
 layer_install() {
     test -f /tmp/archive/boost_1_67_0.tar.gz &&
     wget https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.tar.gz -P /tmp/archive/boost
-    pushd /tmp/source/boost
     mkdir -p /tmp/source/boost 2>/dev/null
     mkdir -p /tmp/build/boost 2>/dev/null
+    pushd /tmp/source/boost
     test ! -d /tmp/source/boost/boost_1_67_0 && tar -zxvf /tmp/archive/boost/boost_1_67_0.tar.gz -C /tmp/source/boost
     test -z "$(ls -A /tmp/build/boost)" && cp -a /tmp/source/boost/boost_1_67_0/. /tmp/build/boost
     pushd /tmp/build/boost
     ./bootstrap.sh --prefix=/usr/local
-    sudo ./b2 --prefix=/usr/local \
+    echo sudo ./b2 --prefix=/usr/local \
          --layout=system \
          -a \
          -j8 \
