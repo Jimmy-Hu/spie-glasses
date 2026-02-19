e562337004d9ded9de7eeccbb6314bf291ad70ab
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-02-19 19:35:45 +0800

    Update c3.cpp
    Update c3.cpp

--- spike/c3.cpp
+++ spike/c3.cpp
@@ -74,7 +74,7 @@ private:
         {
             boost::asio::async_connect(
                 m_socket, endpoint_iterator,
-                [this](boost::system::error_code const & ec, tcp::resolver::iterator)
+                [this](boost::system::error_code const & ec, tcp::resolver::results_type)
                 {
                     auto h = [](boost::system::error_code const & ec, msgpack::object_handle oh){
                         if (!ec) std::cerr << "[info][response] object=" << oh.get() << std::endl;
