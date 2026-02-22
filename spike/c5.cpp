6c7a676e3f8ec954f58f731a160f717c0e5d90be
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-02-22 14:27:23 +0800

    Update c5.cpp
    Update c5.cpp

--- spike/c5.cpp
+++ spike/c5.cpp
@@ -78,7 +78,7 @@ private:
         //             m_socket.close();
         //     });
     // }
-    void do_connect(tcp::resolver::iterator endpoint_iterator)
+    void do_connect(tcp::resolver::results_type endpoint_iterator)
         {
             boost::asio::async_connect(
                 m_socket, endpoint_iterator,
