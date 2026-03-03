2282cfd70015522a09abccf99b2a209802570247
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-03-03 20:52:59 +0800

    Update asio_msgpack.bak.hpp
    Update asio_msgpack.bak.hpp

--- spike/include/api/asio_msgpack.bak.hpp
+++ spike/include/api/asio_msgpack.bak.hpp
@@ -153,7 +153,7 @@ namespace api {
         ValueT & val,
         ReadHandlerT handler)
     {
-        s.get_io_service().post(
+        s.get_executor().context().post(
             [&s,&unp,&val,h=std::move(handler)]() mutable
             {
                 detail::do_async_read_msgpack(
