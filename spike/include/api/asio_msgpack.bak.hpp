f14e2db4994f9839fc1c2c116932f23475c1d4f3
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-03-15 19:16:28 +0800

    Update asio_msgpack.bak.hpp
    Update asio_msgpack.bak.hpp

--- spike/include/api/asio_msgpack.bak.hpp
+++ spike/include/api/asio_msgpack.bak.hpp
@@ -312,7 +312,7 @@ namespace api {
             m_reader.async_read(rv, std::move(rh));
             m_rq.pop_front();
             if (!m_rq.empty())
-                m_reader.get_io_service().post(std::bind(&this->do_read,this));
+                m_reader.get_executor().context().post(std::bind(&this->do_read,this));
         }
         reader_type m_reader;
         read_queue_type m_rq;
