a59fc97f7958467c001f06b26d6a1db72007409c
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-04-18 12:57:52 +0800

    Update asio_msgpack.hpp
    Update asio_msgpack.hpp

--- src/api/asio_msgpack.hpp
+++ src/api/asio_msgpack.hpp
@@ -161,7 +161,7 @@ namespace api {
         ValueT const & val,
         WriteHandlerT handler)
     {
-        s.get_io_service().post(
+        s.get_executor().context().post(
             [&s,&oh,handler=std::move(handler)]
             (){
                 std::unique_ptr<msgpack::sbuffer> sbuf;
