4a1acd1bb6ea886470abc5feaac1ec9876e136a6
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-05-09 18:15:51 +0800

    Update server.hpp
    Update server.hpp

--- src/server/server.hpp
+++ src/server/server.hpp
@@ -139,7 +139,7 @@ public:
     }
 
     void close() {
-        m_acceptor.get_io_service().post(
+        m_acceptor.get_executor().context().post(
             [this](){
                 for (auto kv : m_mgr) {
                     kv.first->close();
