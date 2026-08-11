07304c5fa48cea8906cb6cb68ee71882e318d4cb
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-08-11 22:43:53 +0800

    Update session.hpp
    Update session.hpp

--- src/server/session.hpp
+++ src/server/session.hpp
@@ -105,7 +105,7 @@ public:
         using namespace std::placeholders;
         auto sp = this->shared_from_this();
         if (!sp) return;
-        m_socket.get_executor().context().post([sp](){
+        boost::asio::post(m_socket.get_executor(), [sp](){
                 sp->m_stop = true;
                 sp->m_msgio.post("image_stop", std::make_tuple());
             });
