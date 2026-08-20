a6dca8e9b1ee0b26ceb15590cb7722c837ac9410
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-08-21 02:55:14 +0800

    Update build.sh
    Update build.sh

--- build.sh
+++ build.sh
@@ -119,3 +119,4 @@ echo "==========================================================================
 echo ">>> Build completed successfully!"
 echo ">>> Executables can be found in: $BUILD_DIR/export/bin/"
 echo "=============================================================================="
+
