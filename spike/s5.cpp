3cf77c1186d911048378702633d3c6e6622455bb
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-09-18 21:04:37 +0800

    Update s5.cpp
    Update s5.cpp

--- spike/s5.cpp
+++ spike/s5.cpp
@@ -226,8 +226,8 @@ private:
         // m_dis.async_dispatch();
     // }
     void do_exit() {
-        m_socket.get_executor().context().post(
-            [this]() mutable
+        boost::asio::post(
+            m_socket.get_executor(), [this]() mutable
             {
                 auto sp = this->shared_from_this();
                 sp->m_sessions.erase(sp);
