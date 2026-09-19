581d7245f9aab4ca914a5d5835ee8ac772f3f58c
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-09-19 19:32:12 +0800

    Update s5.cpp
    Update s5.cpp

--- spike/s5.cpp
+++ spike/s5.cpp
@@ -260,8 +260,8 @@ public:
             do_accept();
         }
     void get_focus_session() {
-        m_acceptor.get_executor().context().post(
-            [this](){
+        boost::asio::post(
+            m_acceptor.get_executor(), [this](){
                 auto sp = this->m_wp.lock();
                 std::cerr << "[info] #" << sp << std::endl;
             });
