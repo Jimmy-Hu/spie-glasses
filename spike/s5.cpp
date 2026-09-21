83ef5b9ea5fdfb4bd0413fafe72ef82612721408
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-09-21 15:17:22 +0800

    Update s5.cpp
    Update s5.cpp

--- spike/s5.cpp
+++ spike/s5.cpp
@@ -296,8 +296,8 @@ public:
             });
     }
     void list_sessions() {
-        m_acceptor.get_executor().context().post(
-            [this](){
+        boost::asio::post(
+            m_acceptor.get_executor(), [this](){
                 for (auto & session : m_sessions) {
                     std::cerr << "[info] #" << session << "\n";
                 }
