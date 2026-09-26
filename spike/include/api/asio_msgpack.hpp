56f2338ea7d0b71a1c7bcc4564572c7cc0d0ddf9
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-09-26 18:29:36 +0800

    Update asio_msgpack.hpp
    Update asio_msgpack.hpp

--- spike/include/api/asio_msgpack.hpp
+++ spike/include/api/asio_msgpack.hpp
@@ -93,7 +93,8 @@ namespace api {
         ValueT const & val,
         WriteHandlerT handler)
     {
-        s.get_executor().context().post(
+        boost::asio::post(
+            s.get_executor(),
             [&s,h=std::move(handler),&val] () mutable
             {
                 auto sbuf = std::make_shared<msgpack::sbuffer>();
